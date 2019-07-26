80c90776c8ed7fd8ff28a4f19f42988f1aed0cd8
Emma Ramirez <ramirez.emma.g@gmail.com>
2019-07-26 14:09:16 -0600

    fix: re-compile (my bad)

--- gen1.js
+++ gen1.js
@@ -41,21 +41,21 @@ const checksum = (data) => {
     return checksum_n;
 };
 const TYPE = {
-    0x00: 'NORMAL',
-    0x01: 'FIGHTING',
-    0x02: 'FLYING',
-    0x03: 'POSION',
-    0x04: 'GROUND',
-    0x05: 'ROCK',
-    0x07: 'BUG',
-    0x08: 'GHOST',
-    0x14: 'FIRE',
-    0x15: 'WATER',
-    0x16: 'GRASS',
-    0x17: 'ELECTRIC',
-    0x18: 'PSYCHIC',
-    0x19: 'ICE',
-    0x1A: 'DRAGON'
+    0x00: 'Normal',
+    0x01: 'Fighting',
+    0x02: 'Flying',
+    0x03: 'Poison',
+    0x04: 'Ground',
+    0x05: 'Rock',
+    0x07: 'Bug',
+    0x08: 'Ghost',
+    0x14: 'Fire',
+    0x15: 'Water',
+    0x16: 'Grass',
+    0x17: 'Electric',
+    0x18: 'Psychic',
+    0x19: 'Ice',
+    0x1A: 'Dragon'
 };
 const convertWithCharMap = (buf) => {
     let str = [];
